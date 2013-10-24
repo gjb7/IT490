d6a5b094e4ef9f01e3a7ecc5efc28c36c1d073b5
Grant Butler <butler.grant.j@gmail.com>
2013-10-24 00:32:17 -0400

    Added routing methods.

--- inc/IT490/Router.php
+++ inc/IT490/Router.php
@@ -15,5 +15,60 @@
 		public function serve() {
 			$this->slim->run();
 		}
+		
+		public function controller($path, $controller) {
+			$this->get($path, array($controller, 'index'));
+			$this->get($path . '/new', array($controller, 'new'));
+			$this->post($path, array($controller, 'create'));
+			$this->get($path . '/:id', array($controller, 'show'));
+			$this->get($path . '/:id/edit', array($controller, 'edit'));
+			$this->put($path . '/:id', array($controller, 'update'));
+			$this->delete($path . '/:id', array($controller, 'delete'));
+		}
+		
+		public function get($path, $action) {
+			$this->route('GET', $path, $action);
+		}
+		
+		public function post($path, $action) {
+			$this->route('POST', $path, $action);
+		}
+		
+		public function put($path, $action) {
+			$this->route('PUT', $path, $action);
+		}
+		
+		public function delete($path, $action) {
+			$this->route('DELETE', $path, $action);
+		}
+		
+		public function route($method, $path, $action) {
+			$method = strtolower($method);
+			$self = $this;
+			
+			$this->slim->{$method}($path, function() use ($action, $self) {
+				$self->performAction($action, func_get_args());
+			});
+		}
+		
+		private function performAction($action, $args) {
+			if (is_array($action)) {
+				$this->performController($action, $args);
+			}
+			else if (is_callable($action)) {
+				$this->performCallback($action, $args);
+			}
+		}
+		
+		private function performController($controller, $args) {
+			$class = $controller[0];
+			$method = $controller[1];
+			$instance = new $class();
+			call_user_func_array(array($instance, $method), $args);
+		}
+		
+		private function performCallback($callback, $args) {
+			call_user_func_array($callback, $args);
+		}
 	}
 ?>
