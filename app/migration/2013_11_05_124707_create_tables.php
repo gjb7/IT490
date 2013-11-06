a2e06506b0906a4887c9c60714261ba09c05f069
Grant J. Butler <butler.grant.j@gmail.com>
2013-11-06 09:05:29 -0500

    Updated migration file for new format.

---
+++ app/migration/2013_11_05_124707_create_tables.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class CreateTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Capsule::schema()->create('customers', function ($table) {
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('address');
+			$table->string('city');
+			$table->string('state');
+			$table->string('zip');
+			$table->timestamps();
+		});
+		
+		Capsule::schema()->create('items', function ($table) {
+			$table->increments('id');
+			$table->string('name')->unique();
+			$table->integer('quantity');
+			$table->float('price');
+		});
+		
+		Capsule::schema()->create('orders', function ($table) {
+			$table->increments('id');
+			$table->integer('customer_id')->unsigned();
+			$table->float('amount_due');
+			$table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+		});
+		
+		Capsule::schema()->create('item_order', function ($table) {
+			$table->integer('order_id')->unsigned();
+			$table->integer('item_id')->unsigned();
+			$table->foreign('order_id')->references('id')->on('orders');
+			$table->foreign('item_id')->references('id')->on('items');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Capsule::schema()->drop('customers');
+		Capsule::schema()->drop('items');
+		Capsule::schema()->drop('orders');
+		Capsule::schema()->drop('item_order');
+	}
+
+}
