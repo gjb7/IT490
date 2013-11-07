a48060db781f55cf8083b3725d997ebb0637ad51
Grant J. Butler <butler.grant.j@gmail.com>
2013-11-07 11:38:50 -0500

    Set fillable for mass assignment.

--- app/model/Customer.php
+++ app/model/Customer.php
@@ -1,5 +1,7 @@
 <?php
 	class Customer extends Illuminate\Database\Eloquent\Model {
+		protected $fillable = array('first_name', 'last_name', 'address', 'city', 'state', 'zip');
+		
 		public function order() {
 			return $this->hasMany('Order');
 		}
