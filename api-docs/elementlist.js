3e4b359bd521c185fb880ae802604d26edc87eb0
Grant J. Butler <butler.grant.j@gmail.com>
2013-11-14 14:28:45 -0500

    Updated API docs.

--- api-docs/elementlist.js
+++ api-docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","app()"],["c","IT490\\Application"],["c","IT490\\Router"],["f","path()"]];
+ApiGen.elements = [["c","Exception"],["f","flash()"],["f","flash_errors()"],["c","IT490\\Application"],["c","IT490\\Exception\\RendererNotFoundException"],["c","IT490\\Exception\\ValidatorException"],["c","IT490\\HTMLBuilder"],["c","IT490\\Model"],["c","IT490\\Renderer"],["c","IT490\\Renderer\\HTMLRenderer"],["c","IT490\\Renderer\\JSONRenderer"],["c","IT490\\RendererManager"],["c","IT490\\Router"],["c","IT490\\TwigExtension"],["c","IT490\\URLGenerator"],["c","IT490\\Validator"],["f","path()"],["f","redirect_to_action()"]];
